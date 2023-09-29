4f0eb2bc1ad7473a3509976f89b8fd6d8dac59e0
bhoon <toltolyibh@gmail.com>
2023-09-29 19:00:21 +0900

    error revise11

--- src/main/java/com/example/dockertest/controller/MainController.java
+++ src/main/java/com/example/dockertest/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String main() {
-        return "dockermain12345553424";
+        return "asdf";
     }
 }
