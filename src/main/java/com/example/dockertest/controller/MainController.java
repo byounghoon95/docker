ab6cda1628d314bfd29d4019e0a3d247adf5b2b5
bhoon <toltolyibh@gmail.com>
2023-09-29 19:27:51 +0900

    error revise13

--- src/main/java/com/example/dockertest/controller/MainController.java
+++ src/main/java/com/example/dockertest/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String main() {
-        return "asdf123";
+        return "asdf12445563";
     }
 }
